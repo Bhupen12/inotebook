3e85ce7241bd30b4b8e2e95df4e98d88e67791e4
Bhupen <bhupensahoo200127@gmail.com>
2023-09-10 18:40:38 +0530

    Validatior added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //Create a User using :POST "api/auth/". Doesn't require Auth
-router.post("/", (req, res) => {
-  try {
-    const userData = User(req.body);
-
-    res.status(200).send({ status: "Success", userData });
-  } catch (error) {
-    console.log("something went wrong", error);
-    res.status(500).send("something went wrong!!");
+router.post(
+  "/",
+  [
+    check("name", "Name shouldn't be null").notEmpty(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password should be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+    try {
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then((user) => res.json(user));
+    } catch (error) {
+      console.log("something went wrong", error);
+      res.status(500).send("something went wrong!!");
+    }
   }
-});
+);
 
 module.exports = router;
