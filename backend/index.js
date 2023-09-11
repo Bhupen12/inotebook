8a9ea7441faec6b11fe2e1bf509d3edc3216ff2f
Bhupen <bhupensahoo200127@gmail.com>
2023-09-11 22:02:18 +0530

    check duplicate user

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require("express");
 
-const connectToMongo = require('./db');
+const connectToMongo = require("./db");
 connectToMongo();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000;
 
 // Middleware
 app.use(express.json());
 
 // Routing
-app.use('/api/note', require('./routes/note'));
-app.use('/api/auth', require('./routes/auth'));
+app.use("/api/note/", require("./routes/note"));
+app.use("/api/auth/", require("./routes/auth"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const { check, validationResult } = require("express-validator");
 
 //Create a User using :POST "api/auth/". Doesn't require Auth
 router.post(
-  "/",
+  "/createuser",
   [
     check("name", "Name shouldn't be null").notEmpty(),
     check("email", "Email is required").isEmail(),
@@ -13,20 +13,26 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
     }
+
     try {
-      User.create({
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).send("User already exists");
+      }
+
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then((user) => res.json(user));
-    } catch (error) {
-      console.log("something went wrong", error);
-      res.status(500).send("something went wrong!!");
+      });
+      res.send(user);
+    } catch (err) {
+      res.status(500).send("Some error occured");
     }
   }
 );
