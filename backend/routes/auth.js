97c8af408595f4090a6e48d67fd85bfd6ef06a98
Bhupen <bhupensahoo200127@gmail.com>
2023-09-11 22:59:49 +0530

    hashcode and JWT Token added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const JWT_TOKEN = "mynameisbhupen";
 
 //Create a User using :POST "api/auth/". Doesn't require Auth
 router.post(
@@ -25,12 +29,22 @@ router.post(
         return res.status(400).send("User already exists");
       }
 
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.send(user);
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_TOKEN);
+      res.json({ authToken });
     } catch (err) {
       res.status(500).send("Some error occured");
     }
