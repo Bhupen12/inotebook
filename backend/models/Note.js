45d941304e293d04675db7798bf34b6271c669ac
Bhupen <bhupensahoo200127@gmail.com>
2023-09-13 00:08:01 +0530

    add notes functionality

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
 const NoteSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description:{
-        type: String,
-        required: true,
-    },
-    tag:{
-        type: String,
-        default: "General",
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now,
-    },
-  });
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    default: "General",
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("note", NoteSchema);
--- backend/routes/note.js
+++ backend/routes/note.js
@@ -1,17 +1,48 @@
-const express = require('express');
+const express = require("express");
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.json([
-        {
-            id: 1,
-            text: 'Hello'
-        },
-        {
-            id: 2,
-            text: 'Bye'
-        },
-    ]);
+router.get("/fetchnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.send(notes);
+  } catch (error) {
+    console.log(error);
+    res.send({ error: error.message });
+  }
 });
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    check("title", "Must be minimum of 3 character").isLength({ min: 3 }),
+    check("description", "Must be minimum of 5 character").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).send({ error: errors.array() });
+      }
+
+      const { title, description, tag } = req.body;
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      console.log(savedNote);
+      res.status(200).json(savedNote);
+    } catch (error) {
+      console.log(error);
+      res.status(401).send({ error: error.array() });
+    }
+  }
+);
 
 module.exports = router;
