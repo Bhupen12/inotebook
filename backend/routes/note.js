2d39a12a371ef80f3d5fd2e1da10e2bf49c7f8ac
Bhupen <bhupensahoo200127@gmail.com>
2023-09-13 22:20:51 +0530

    Update Note Endpoint

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -4,6 +4,7 @@ const Note = require("../models/Note");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
+// Route 1: Get all the notes using: GET "/api/note/fetchallnotes". Login required
 router.get("/fetchnotes", fetchuser, async (req, res) => {
   try {
     const notes = await Note.find({ user: req.user.id });
@@ -14,6 +15,7 @@ router.get("/fetchnotes", fetchuser, async (req, res) => {
   }
 });
 
+// Route 2: Add a new note using: POST "/api/note/addnote". Login required
 router.post(
   "/addnote",
   fetchuser,
@@ -45,4 +47,40 @@ router.post(
   }
 );
 
+// // Route 3: Update a note using: POST "/api/note/updatenote/:id". Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+    const newNote = {};
+
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      res.status(404).send("Not found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+    res.json({ note });
+  } catch (error) {
+    console.log("Catch: ", error);
+    res.send({ error: error.message });
+  }
+});
 module.exports = router;
